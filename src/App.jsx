905a8a33f9c7027eee2d4c2b0f0df91836969af0
swapnilshikha <smegha.3004@gmail.com>
2024-07-28 10:35:19 +0530

    Login System

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import SignIn from './components/SignIn'
+import SignUp from './components/SignUp'
+
+const App = () => {
+  return (
+    <div>
+      <SignIn />
+      <br />
+      <br />
+      <SignUp />
+    </div>
+  )
+}
+
+export default App
